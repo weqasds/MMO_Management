88dda0d892f3cbd7bd31001f5f220c1f50847429
wzdstc <1320934212@qq.com>
2023-06-09 18:40:37 +0800

    添加菜单

--- CUI/Header/Util/Menu.h
+++ CUI/Header/Util/Menu.h
@@ -2,5 +2,29 @@
 #include"..\..\pch.h"
 class Menu
 {
-};
 
+	class showmenu
+	{
+	public:
+		void main_menu();//主菜单
+		void  manipuation_A1();//数据操纵功能
+		void reduction_A2();//数据还原功能
+		void KZUSE_A3();//控制使用A3
+		void APPLY_A4();//申请
+		void  manipuation_B1();//数据操纵功能B1
+		void reduction_B2();//数据还原功能B2
+		void KZUSE_B3();//控制使用B3
+		void maintenance_B4();//维护B4
+		void migrate_B5();//数据迁移B5
+		void  manipuation_C1();//数据操纵功能C1
+		void reduction_C2();//数据还原功能C2
+		void KZUSE_C3();//控制使用C3
+		void maintenance_C4();//维护C4
+		void migrate_C5();//数据迁移C5
+		void innew_C6();//增加新功能C6
+		void topast_C7();//删除旧功能C7
+
+	};
+
+
+};
